35b4997606d53fa29302273d04f7e98caed10275
NightThunder <night.thunderx@gmail.com>
2019-06-06 22:18:20 +0200

    process message

--- assistant4discord/nlp_tasks/message_processing.py
+++ assistant4discord/nlp_tasks/message_processing.py
@@ -1,1 +1,16 @@
+import re
 
+
+def remove_punctuation(sent):
+    punct = r'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' + '’`”“'
+    text_nopunct = ''.join(char for char in sent if char not in punct)
+    return text_nopunct
+
+
+def replace_numbers(sent):
+    text_nonum = re.sub(r'[0-9]+', 'stevilka', sent)
+    return text_nonum
+
+
+def process_message(message):
+    return replace_numbers(remove_punctuation(message))
