c83270214065a5eb5483585aa7727db052f1b86b
NightThunder <night.thunderx@gmail.com>
2019-06-07 22:31:05 +0200

    fix this

--- assistant4discord/assistant/commander.py
+++ assistant4discord/assistant/commander.py
@@ -6,5 +6,10 @@ class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
     async def ping(self, ctx):
         await ctx.send('{} ms'.format(round(self.bot.latency * 1000)))
+
+
+# TODO fix this
+# probably with MyClient: https://github.com/Rapptz/discord.py/blob/master/examples/background_task.py
