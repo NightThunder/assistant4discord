0c3da431089e208992bdb682f9e10e96935f3091
NightThunder <night.thunderx@gmail.com>
2019-06-08 22:22:19 +0200

    all new and shiny

--- assistant4discord/assistant/commander.py
+++ assistant4discord/assistant/commander.py
@@ -1,15 +1,55 @@
-from discord.ext import commands
+from assistant4discord.nlp_tasks.similarity import Similarity
+from importlib import import_module
+import os
+import inspect
 
 
-class Ping(commands.Cog):
+class Commander:
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, dir_path=None):
+        self.commands = self.get_commands(dir_path)
+        self.calls = self.get_command_calls()
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('{} ms'.format(round(self.bot.latency * 1000)))
+    @staticmethod
+    def get_commands(dir_path):
+        """Get all commands from directory.
+
+        Args:
+            dir_path: directory path
+
+        Returns: list of command objects
+        """
+        command_dct = {}
+
+        if not dir_path:
+            dir_path = os.path.dirname(os.path.realpath(__file__)) + '/commands'
+
+        file_lst = os.listdir(dir_path)
+
+        for file in file_lst:
+            if file.endswith('.py') and '__init__' not in file:
+                module = import_module('.{}'.format(file[:-3]), package='commands')
+                for name, obj in inspect.getmembers(module):
+                    if inspect.isclass(obj):
+                        command_dct['{}'.format(name)] = obj
+
+        return command_dct
+
+    def get_command_calls(self):
+        command_calls = []
+
+        for command_str, command in self.commands.items():
+            if command_str != 'Basic':
+                command_calls.append(command().call)
+
+        return command_calls
+
+    def message_to_command(self, message: str, client: object):
+        self.commands['Basic'](message, client)
+
+        sim = Similarity('5days_askreddit_model.kv').message_x_command_sim(message, self.calls)
+        return sim
 
 
-# TODO fix this
-# probably with MyClient: https://github.com/Rapptz/discord.py/blob/master/examples/background_task.py
+C = Commander()
+print(C.message_to_command('whats my ping', 'client'))
