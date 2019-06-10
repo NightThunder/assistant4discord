1e4d6dcef7f45c79be647bd7b9c413369d6d59d8
NightThunder <night.thunderx@gmail.com>
2019-06-10 22:11:59 +0200

    fixed

--- assistant4discord/assistant/commander.py
+++ assistant4discord/assistant/commander.py
@@ -8,10 +8,9 @@ import numpy as np
 class Commander:
 
     def __init__(self, client, model_name, dir_path=None):
-        self.client = client
+        self.client = client                                                # discord client from MyClient
         self.commands = self.get_commands(dir_path)                         # dict of commands from commands dir, {command: 'command text', ...}
-        self.calls = self.get_command_calls()[0]                            # list of commands texts, [command text, ...]
-        self.basic = self.get_command_calls()[1]    # TODO make this better
+        self.calls = self.get_command_calls()                               # list of commands texts, [command text, ...]
         self.sim = Similarity(model_name)                                   # w2v model class
         self.command_vectors = self.sim.get_sentence2vec(self.calls)        # calculate command text sentence vectors from self.calls
 
@@ -33,9 +32,10 @@ class Commander:
 
         for file in file_lst:
             if file.endswith('.py') and '__init__' not in file:
-                module = import_module('.{}'.format(file[:-3]), package='commands')     # TODO fix this if ran from main
+                module = import_module('assistant4discord.assistant.commands.{}'.format(file[:-3]))
                 for name, obj in inspect.getmembers(module):
                     if inspect.isclass(obj):
+                        print('imported command: {}'.format(name))
                         command_dct['{}'.format(name)] = obj
 
         return command_dct
@@ -44,25 +44,22 @@ class Commander:
         command_calls = []
 
         for command_str, command in self.commands.items():
-            if command_str != 'Basic':
-                command_calls.append(command().call)
-            else:
-                basic = command(client=self.client)
+            command_calls.append(command().call)
 
-        return command_calls, basic
+        return command_calls
 
     def message_to_command(self, message: str) -> object:
-        self.basic.message = message
 
-        sim_arr = self.sim.message_x_command_sim(message, self.command_vectors, saved_command_vectors=True)
+        sim_arr = self.sim.message_x_command_sim(message.content[22:], self.command_vectors, saved_command_vectors=True)
 
-        # TODO do something if max(sim_arr) < some number
+        print('command calls: {} \nsimilarities: {}'.format(self.calls, sim_arr))
 
         picked_command_str = self.calls[int(np.argmax(sim_arr))]
 
         for command_str, command in self.commands.items():
-            if command.__name__ != 'Basic' and command().call == picked_command_str:
-                return command()
+            if command().call == picked_command_str:
+                command_obj = command(client=self.client, message=message, similarity=self.sim)
+                return command_obj
 
         return None
 
