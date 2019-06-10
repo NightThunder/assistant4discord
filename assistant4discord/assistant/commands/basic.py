730f41bf3f0fa208f46769681cd7ccd74dbec251
NightThunder <night.thunderx@gmail.com>
2019-06-10 22:11:35 +0200

    no longer using subclasses

--- assistant4discord/assistant/commands/basic.py
+++ assistant4discord/assistant/commands/basic.py
@@ -1,27 +1,26 @@
 import asyncio
+from assistant4discord.nlp_tasks.message_processing import word2vec_input
 
 
-class Basic:
+class Ping:
 
-    def __init__(self, message=None, client=None):
-        self.message = message
-        self.client = client
+    def __init__(self, **kwargs):
+        if kwargs:
+            self.client = kwargs['client']
+            self.message = kwargs['message']
 
-
-class Ping(Basic):
-
-    def __init__(self, *args):
-        super().__init__(*args)
         self.call = 'ping'
 
     async def doit(self):
         await self.message.channel.send('{} ms'.format(round(self.client.latency * 1000)))
 
 
-class After10(Basic):
+class After10:
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, **kwargs):
+        if kwargs:
+            self.message = kwargs['message']
+
         self.call = 'sleep'
 
     async def doit(self):
@@ -29,3 +28,22 @@ class After10(Basic):
         await asyncio.sleep(10)
         await self.message.channel.send('woken up after 10')
 
+
+class WordSim:
+
+    def __init__(self, **kwargs):
+        if kwargs:
+            self.message = kwargs['message']
+            self.sim = kwargs['similarity']
+
+        self.call = 'word similarity'
+
+    async def doit(self):
+        sent = word2vec_input(self.message.content[22:])
+        print(sent)
+        similarity = self.sim.model.similarity(sent[-1], sent[-2])
+
+        await self.message.channel.send(str(similarity))
+
+
+# TODO fix similarities questions that have same input as command call (input: word similarity ping, latency)
