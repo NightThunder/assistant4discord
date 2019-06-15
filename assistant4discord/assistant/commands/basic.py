d13b7dc4b9417e3cf2ba3df5bb72826c60224f22
NightThunder <night.thunderx@gmail.com>
2019-06-15 19:27:59 +0200

    Help, MostSimilarWords

--- assistant4discord/assistant/commands/basic.py
+++ assistant4discord/assistant/commands/basic.py
@@ -17,12 +17,45 @@ class Master:
         self.sim = similarity
 
 
+class Help(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'help'
+        self.commands = None
+
+    async def doit(self):
+
+        message = self.message.content[22:]
+
+        if len(word2vec_input(message)) > 1:
+            for i, (command_str, command) in enumerate(self.commands.items()):
+                if command_str in message.lower() and command_str != 'help':
+                    await self.message.channel.send(command.help)
+                    break
+
+                if i == len(self.commands) - 1:
+                    await self.message.channel.send('Command not found!')
+
+        else:
+            command_str = 'My commands: '
+            for i, command_str_ in enumerate(self.commands.keys()):
+                if i < len(self.commands) - 1:
+                    command_str += command_str_.lower() + ', '
+                else:
+                    command_str += command_str_.lower()
+
+            command_str += '\nType help <command> for more info!'
+
+            await self.message.channel.send(command_str)
+
+
 class Ping(Master):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.call = 'ping'
+        self.help = 'Ping discord server and return response time in ms.'
 
     async def doit(self):
         await self.message.channel.send('{} ms'.format(round(self.client.latency * 1000)))
@@ -33,6 +66,7 @@ class After10(Master):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.call = 'sleep'
+        self.help = 'asyncio.sleep() test'
 
     async def doit(self):
         await self.message.channel.send('sleeping for 10')
@@ -40,14 +74,33 @@ class After10(Master):
         await self.message.channel.send('woken up after 10')
 
 
-class WordSim(Master):
+class Word2WordSim(Master):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.call = 'similarity'
+        self.help = 'Returns cosine similarity between last two words in message.\nNote: only works with keyword similarity.'
 
     async def doit(self):
         sent = word2vec_input(self.message.content[22:])
         similarity = self.sim.model.similarity(sent[-1], sent[-2])
 
         await self.message.channel.send(str(similarity))
+
+
+class MostSimilarWords(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'most similar'
+        self.help = 'Return 50 most similar words to last word in message.\nNote: only works with keyword most similar.'
+
+    async def doit(self):
+        sent = word2vec_input(self.message.content[22:])
+        sims = self.sim.model.similar_by_word(sent[-1], topn=50)
+
+        sim_str = ''
+        for i in sims:
+            sim_str += '{}: {:.2f} '.format(i[0], i[1])
+
+        await self.message.channel.send(sim_str)
