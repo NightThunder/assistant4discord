d134af7f7e5d76a9ab0640f6e4f80d65d68d847d
NightThunder <night.thunderx@gmail.com>
2019-06-15 19:28:06 +0200

    fixes

--- assistant4discord/discord_api/discord_client.py
+++ assistant4discord/discord_api/discord_client.py
@@ -1,47 +1,38 @@
 import discord
 import logging
+from assistant4discord.data.logger import setup_logger
 from assistant4discord.assistant.messenger import Messenger
 
 
+logging.basicConfig(level=logging.INFO)
+chat_logger = setup_logger(log_name='chat.log')
+
+
 class MyClient(discord.Client):
+
+    def __init__(self, messenger=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.messenger = messenger
 
     async def on_ready(self):
         print('Logged in as: {}, {}'.format(self.user.name, self.user.id))
 
     async def on_message(self, message):
-        chat_logger.info('channel: {}, author full name: {}, author name: {}, content: {}'.format(message.channel, message.author, message.author.name, message.content))
+        chat_logger.info('channel: {}, author full name: {}, author name: {}, content: {}'
+                         .format(message.channel, message.author, message.author.name, message.content))
 
         if message.author == self.user:
             return
 
         if message.content.startswith('<@{}>'.format(self.user.id)):
-            await M.message_to_command(message).doit()
+            M = self.messenger.message_to_command(message)
+            if M:
+                await M.doit()
+            else:
+                await message.channel.send('Error: not implemented')
 
 
-def setup_chat_logger(name=__name__, log_name='new.log', level=logging.DEBUG, format='%(asctime)s: %(levelname)s: %(message)s'):
-    from pathlib import Path
-
-    log_file = str(Path(__file__).parents[1]) + '/data/' + log_name
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    formatter = logging.Formatter(format)
-
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-
-    return logger
-
-
-logging.basicConfig(level=logging.INFO)
-chat_logger = setup_chat_logger(log_name='chat.log')
-
-my_token = open('../token.txt', 'r').read()
-client = MyClient()
-
-M = Messenger(client=client, model_name='5days_askreddit_model.kv')
-
-client.run(my_token)
+def run(model_name, my_token):
+    client = MyClient()
+    client.messenger = Messenger(client=client, model_name=model_name)
+    client.run(my_token)
