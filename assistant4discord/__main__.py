e1a05289028b22deb737da45ff05686bf8547a2b
NightThunder <37780539+NightThunder@users.noreply.github.com>
2019-06-15 22:53:45 +0200

    Update __main__.py

--- assistant4discord/__main__.py
+++ assistant4discord/__main__.py
@@ -2,5 +2,5 @@ from assistant4discord.discord_api.discord_client import run
 
 
 if __name__ == '__main__':
-    my_token = open('../token.txt', 'r').read()
+    my_token = open('./token.txt', 'r').read()
     run('5days_askreddit_model.kv', my_token)
