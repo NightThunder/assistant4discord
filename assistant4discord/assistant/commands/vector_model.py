c793c29ac3572b81e12be92ce592b1066ed255b6
NightThunder <night.thunderx@gmail.com>
2019-06-17 20:54:46 +0200

    name fix

--- assistant4discord/assistant/commands/vector_model.py
+++ assistant4discord/assistant/commands/vector_model.py
@@ -1,4 +1,4 @@
-from assistant4discord.assistant.commands.basic import Master
+from assistant4discord.assistant.commands.master.master_class import Master
 from assistant4discord.nlp_tasks.message_processing import word2vec_input
 
 
