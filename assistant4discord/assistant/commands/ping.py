d910dc022ccb9d6cc8c25481b96aaa44706e9c61
NightThunder <night.thunderx@gmail.com>
2019-06-18 12:54:38 +0200

    ping command

---
+++ assistant4discord/assistant/commands/ping.py
@@ -0,0 +1,12 @@
+from assistant4discord.assistant.commands.master.master_class import Master
+
+
+class Ping(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'ping'
+        self.help = 'Ping discord server and return response time in ms.'
+
+    async def doit(self):
+        await self.message.channel.send('{} ms'.format(round(self.client.latency * 1000)))
