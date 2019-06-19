29034fdd2a975e4645c174e4613ade866a8d9dba
NightThunder <night.thunderx@gmail.com>
2019-06-19 22:05:01 +0200

    new model

--- assistant4discord/__main__.py
+++ assistant4discord/__main__.py
@@ -3,4 +3,4 @@ from assistant4discord.discord_api.discord_client import run
 
 if __name__ == '__main__':
     my_token = open('./token.txt', 'r').read()
-    run('5days_askreddit_model.kv', my_token)
+    run('model_v1.kv', my_token)
