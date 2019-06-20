052fced9bd6383efff3f30857bcdcd4d48bd3a69
NightThunder <night.thunderx@gmail.com>
2019-06-20 22:39:49 +0200

    command timer

---
+++ assistant4discord/assistant/commands/timer.py
@@ -0,0 +1,110 @@
+import asyncio
+from assistant4discord.nlp_tasks.message_processing import word2vec_input
+from assistant4discord.assistant.commands.master.master_class import Master
+from assistant4discord.assistant.commands.timer_helper.timer_class import Timer
+
+
+times = {'second': 1, 'seconds': 1, 'sec': 1, 's': 1, 'minute': 60, 'minutes': 60, 'min': 60, 'm': 60,
+         'hour': 3600, 'hours': 3600, 'h': 3600, 'day': 86400, 'days': 86400, 'd': 86400, 'week': 604800,
+         'weeks': 604800, 'w': 604800}
+
+
+class TimeIt(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'time stevilka time'
+        self.help = '```***Timer help***\n' \
+                    'Run a command in specified time.\n' \
+                    'Example: time <time when to run> <any command>```'
+        self.all_timers = []
+
+    def remove_dead_timers(self):
+
+        all_active_timers = []
+
+        for timer in self.all_timers:
+            if not timer.task.done():
+                all_active_timers.append(timer)
+
+        self.all_timers = all_active_timers
+
+    async def coro_doit(self, timer):
+
+        await self.message.channel.send('{} {}'.format(timer.future_command.__class__.__name__, str(timer)))
+
+        while True:
+            await asyncio.sleep(timer.time_to_timer)
+            await timer.future_command.doit()
+
+            if timer.every is False:
+                return
+
+    async def doit(self):
+
+        timer = Timer(message=self.message, similarity=self.sim, commands=self.commands, command_vectors=self.command_vectors, calls=self.calls)
+
+        if timer.time_to_timer and timer.future_command:
+            task = self.client.loop.create_task(self.coro_doit(timer))
+            timer.task = task
+            self.all_timers.append(timer)
+        else:
+            await self.message.channel.send('something went wrong')
+
+
+class ShowTimers(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'show timer'
+        self.help = '```***ShowTimers help***\n' \
+                    'Display user\'s active timers.\n```'
+
+    async def doit(self):
+
+        self.commands['TimeIt'].remove_dead_timers()
+        timer_str = ''
+
+        n_timers = 0
+        for i, timer in enumerate(self.commands['TimeIt'].all_timers):
+            if timer.message.author == self.message.author and not timer.task.done():
+                timer_str += '**timer {}:** {} {}\n'.format(i, timer.future_command.__class__.__name__, str(timer))
+                n_timers += 1
+
+        if n_timers != 0:
+            await self.message.channel.send(timer_str)
+        else:
+            await self.message.channel.send('You have no active timers!')
+
+
+class KillTimer(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.call = 'kill timer stevilka'
+        self.help = '```***KillTimer help***\n' \
+                    'Cancels user\'s active timer.\n' \
+                    'Example: cancel timer <timer\'s number shown in ShowTimers>```'
+
+    async def doit(self):
+
+        self.commands['TimeIt'].remove_dead_timers()
+
+        try:
+            to_kill = int(word2vec_input(self.message.content[22:], replace_num=False)[-1])
+        except ValueError:
+            await self.message.channel.send('something went wrong')
+            return
+
+        n_timers = 0
+        for i, timer in enumerate(self.commands['TimeIt'].all_timers):
+            if timer.message.author == self.message.author and not timer.task.done() and i == to_kill:
+                timer.task.cancel()
+                break
+            else:
+                n_timers += 1
+
+        if to_kill > n_timers:
+            await self.message.channel.send('Could not find that timer!')
+        else:
+            await self.message.channel.send('Timer {} canceled!'.format(to_kill))
---
+++ assistant4discord/assistant/commands/timer_helper/timer_class.py
@@ -0,0 +1,94 @@
+from assistant4discord.nlp_tasks.message_processing import word2vec_input
+from assistant4discord.assistant.commands.master.master_class import Master
+import datetime
+import time
+import numpy as np
+
+
+times = {'second': 1, 'seconds': 1, 'sec': 1, 's': 1, 'minute': 60, 'minutes': 60, 'min': 60, 'm': 60,
+         'hour': 3600, 'hours': 3600, 'h': 3600, 'day': 86400, 'days': 86400, 'd': 86400, 'week': 604800,
+         'weeks': 604800, 'w': 604800}
+
+
+class Timer(Master):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.every = self.every_t()
+        (self.time_to_timer, self.time_str, future_command_str) = self.message_filter()
+        self.future_command = self.message_to_command(future_command_str)
+        self.task = None
+
+    def message_to_command(self, message):
+
+        sim_arr = self.sim.message_x_command_sim(' '.join(message), self.command_vectors, saved_command_vectors=True)
+        picked_command_str = self.calls[int(np.argmax(sim_arr))]
+
+        print('message:', ' '.join(message))
+        for i in range(len(self.calls)):
+            print('{}: {:.2f}'.format(self.calls[i], sim_arr[i]))
+
+        if np.max(sim_arr) < 0.3:
+            return None
+
+        for command in self.commands.values():
+            if command.call == picked_command_str:
+                command.message = self.message
+                return command
+
+    def message_filter(self):
+
+        message = word2vec_input(self.message.content[22:], replace_num=False)
+
+        if self.every:
+            message.remove('every')
+
+        future_command_str = message.copy()
+        str_time_to_command = []
+
+        try:
+            time_i = message.index('time')
+            future_command_str.pop(time_i)
+        except ValueError:
+            return
+
+        time_to_command = 0
+        for i, word in enumerate(message[time_i:]):
+
+            if word in times:
+                try:
+                    time_to_command += times[word] * int(message[i + time_i - 1])
+                    for _ in range(2):
+                        t_str = future_command_str.pop(i + time_i - (len(message) - len(future_command_str)))
+                        str_time_to_command.append(t_str)
+
+                    str_time_to_command[-1], str_time_to_command[-2] = str_time_to_command[-2], str_time_to_command[-1]
+
+                except ValueError:
+                    return None
+
+                try:
+                    message[i + time_i + 2]
+                except IndexError:
+                    break
+
+                if message[i + time_i + 2] not in times:
+                    break
+
+        if time_to_command == 0:
+            return None
+        else:
+            return time_to_command, str_time_to_command, future_command_str
+
+    def every_t(self):
+        message = word2vec_input(self.message.content[22:], replace_num=False)
+        if 'every' in message:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        if self.every:
+            return 'set for every: {}'.format(' '.join(self.time_str))
+        else:
+            return 'set for: {}'.format(datetime.datetime.fromtimestamp(int(time.time() + self.time_to_timer)).strftime('%d/%m/%Y @ %H:%M:%S'))
