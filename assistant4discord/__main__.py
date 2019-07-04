ec86e5c2d8ef70ef69bbfaf90ceaecadd867b31f
NightThunder <night.thunderx@gmail.com>
2019-07-04 22:45:52 +0200

    TODO

--- assistant4discord/__main__.py
+++ assistant4discord/__main__.py
@@ -4,3 +4,7 @@ from assistant4discord.discord_api.discord_client import run
 if __name__ == '__main__':
     my_token = open('./token.txt', 'r').read()
     run('model_v1.kv', my_token)
+
+
+# TODO https://aiohttp.readthedocs.io/en/stable/client_reference.html
+# TODO https://stackoverflow.com/questions/35879769/fetching-multiple-urls-with-aiohttp-in-python-3-5
