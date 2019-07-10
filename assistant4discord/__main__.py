b65781f0a70d082b8c73e9bc09a4dadd599390ce
NightThunder <night.thunderx@gmail.com>
2019-07-10 22:25:48 +0200

    chat logging optional

--- assistant4discord/__main__.py
+++ assistant4discord/__main__.py
@@ -3,8 +3,4 @@ from assistant4discord.discord_api.discord_client import run
 
 if __name__ == '__main__':
     my_token = open('./token.txt', 'r').read()
-    run('model_v1.kv', my_token)
-
-
-# TODO https://aiohttp.readthedocs.io/en/stable/client_reference.html
-# TODO https://stackoverflow.com/questions/35879769/fetching-multiple-urls-with-aiohttp-in-python-3-5
+    run(model_name='model_v1.kv', my_token=my_token, log_chat=True)
--- assistant4discord/discord_api/discord_client.py
+++ assistant4discord/discord_api/discord_client.py
@@ -5,21 +5,24 @@ from assistant4discord.assistant.messenger import Messenger
 
 
 logging.basicConfig(level=logging.INFO)
-chat_logger = setup_logger(log_name='chat.log')
 
 
 class MyClient(discord.Client):
 
-    def __init__(self, messenger=None, *args, **kwargs):
+    def __init__(self, messenger=None, log_chat=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.log_chat = log_chat
         self.messenger = messenger
+        self.log_chat = log_chat
+        self.chat_logger = None
 
     async def on_ready(self):
         print('Logged in as: {}, {}'.format(self.user.name, self.user.id))
 
     async def on_message(self, message):
-        chat_logger.info('channel: {}, author full name: {}, author name: {}, content: {}'
-                         .format(message.channel, message.author, message.author.name, message.content))
+        if self.log_chat:
+            self.chat_logger.info('channel: {}, author full name: {}, author name: {}, content: {}'
+                                  .format(message.channel, message.author, message.author.name, message.content))
 
         if message.author == self.user:
             return
@@ -32,7 +35,13 @@ class MyClient(discord.Client):
                 await message.channel.send('Error: not implemented')
 
 
-def run(model_name, my_token):
+def run(model_name, my_token, log_chat):
+
     client = MyClient()
+
+    if log_chat:
+        client.log_chat = log_chat
+        client.chat_logger = setup_logger(log_name='chat.log')
+
     client.messenger = Messenger(client=client, model_name=model_name)
     client.run(my_token)
