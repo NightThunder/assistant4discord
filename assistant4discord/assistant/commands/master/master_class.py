51fe00afbf2964e8301ee4b474dc06acc446992c
NightThunder <night.thunderx@gmail.com>
2019-07-14 21:43:33 +0200

    removed command_vectors

--- assistant4discord/assistant/commands/master/master_class.py
+++ assistant4discord/assistant/commands/master/master_class.py
@@ -1,10 +1,9 @@
 class Master:
 
-    def __init__(self, client=None, message=None, commands=None, similarity=None, command_vectors=None, calls=None):
+    def __init__(self, client=None, message=None, commands=None, similarity=None, calls=None):
         """ Base class for commands."""
         self.client = client
         self.message = message
         self.commands = commands
         self.sim = similarity
-        self.command_vectors = command_vectors
         self.calls = calls
