ba6de92598d44e5112089b796b326249bc34558c
NightThunder <night.thunderx@gmail.com>
2019-07-31 21:32:39 +0200

    environment variables added

--- assistant4discord/__main__.py
+++ assistant4discord/__main__.py
@@ -1,6 +1,11 @@
 from assistant4discord.discord_api.discord_client import run
+import os
+
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+MONGODB_TOKEN = os.environ['MONGODB_TOKEN']
 
 
 if __name__ == "__main__":
-    my_token = open("./token.txt", "r").read()
-    run(method="w2v", model_name="model_v1.kv", my_token=my_token, log_chat=True)
+    # my_token = open("./token.txt", "r").read()
+    run(method="w2v", model_name="model_v1.kv", discord_token=DISCORD_TOKEN, mongodb_token=MONGODB_TOKEN, log_chat=True)
