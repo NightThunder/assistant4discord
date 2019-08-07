ded87fbfdbb143da981177180a9f1da8d9c3a097
NightThunder <night.thunderx@gmail.com>
2019-08-07 22:16:07 +0200

    todo does everything now, __str__ change

--- assistant4discord/assistant/commands/extensions/timer_class.py
+++ assistant4discord/assistant/commands/extensions/timer_class.py
@@ -1,43 +1,81 @@
 from assistant4discord.assistant.commands.master.master_class import Master
-from assistant4discord.nlp_tasks.find_times import sent_time_finder, timestamp_to_utc
+from assistant4discord.nlp_tasks.find_times import sent_time_finder, timestamp_to_utc, convert_sec
 import time
 import numpy as np
 
 
 class Timer(Master):
+
     def __init__(self, **kwargs):
         """
         Other Parameters
         ----------------
-        calls: list of str
-            Get command calls from all commands.
+        name: str
+            Used for identification.
+        run_on_init: bool
+            If True runs once on initialization.
+        use_asyncio: bool
+            True because asyncio and aiohttp are needed.
         time_to_message: int
+            Seconds to message.
         every: bool
-        future_command_str: str
-            User message with command and time in it.
+            True if repeated (do again after sleep).
+        switch: int
+            0 if never ran, 1 if ran once or more.
+        future_command: obj
+            Class name of command to be executed.
         created_on: int
-            When was this initialized
+            When did todo() ran.
+
+        Note
+        ----
+        All None attributes in __init__ are initialized in todo() method.
+
         """
         super().__init__(**kwargs)
-        self.name = 'timers'
+        self.name = "time_it"
         self.run_on_init = True
         self.use_asyncio = True
-        self.calls = None
         self.time_to_message = None
         self.every = None
-        self.future_command_call = None
+        self.switch = 0
+        self.future_command = None
         self.created_on = int(time.time())
 
-    def todo(self):
-        self.calls = self.get_command_calls()
-        (self.time_to_message, self.every, future_command_str) = self.message_filter()
-        self.future_command_call = self.message_to_command(future_command_str)
+    async def todo(self):
 
-    def message_to_command(self, message):
+        if self.switch == 0:
+            (self.time_to_message, self.every, future_command_str) = self.message_filter()
+
+            calls = self.get_command_calls()
+            future_command_call = self.message_to_command(future_command_str, calls)
+
+            if not future_command_call:
+                return None
+
+            for command_str, command in self.commands.items():
+                if command.call == future_command_call:
+                    self.future_command = command_str
+
+            self.switch += 1
+
+        else:
+            chosen_one = self.commands[self.future_command]
+
+            if self.saved_channel:
+                chosen_one.saved_channel = self.saved_channel
+            else:
+                chosen_one.message = self.message
+
+            self.created_on = int(time.time())
+
+            await chosen_one.doit()
+
+    def message_to_command(self, message, calls):
         """ Same as Messenger. """
 
         sim_arr = self.sim.message_x_command_sim(" ".join(message))
-        picked_command_str = self.calls[int(np.argmax(sim_arr))]
+        picked_command_str = calls[int(np.argmax(sim_arr))]
 
         if np.max(sim_arr) < 0.25:
             return None
@@ -76,14 +114,14 @@ class Timer(Master):
         return time_to_command, every, future_command
 
     def __str__(self):
-        """ %d.%m.%Y %H:%M:%S representation."""
         if self.every:
-            return "command: {}\nnext run set for {}".format(
-                self.message.content,
+            return "command: {}\nset every {}\nnext run on: {} utc".format(
+                self.future_command,
+                convert_sec(self.time_to_message),
                 timestamp_to_utc(self.time_to_message + self.created_on),
             )
         else:
-            return "command: {}\nset for {}".format(
-                self.message.content,
+            return "command: {}\nset for {} utc".format(
+                self.future_command,
                 timestamp_to_utc(self.time_to_message + self.created_on),
             )
