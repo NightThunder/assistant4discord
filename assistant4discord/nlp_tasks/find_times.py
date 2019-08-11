5c21d0a6affcccd8e5bbd9b50e135318a21a77e3
NightThunder <night.thunderx@gmail.com>
2019-08-11 21:33:32 +0200

    bug fixes, changed every, examples added

--- assistant4discord/nlp_tasks/find_times.py
+++ assistant4discord/nlp_tasks/find_times.py
@@ -53,8 +53,9 @@ days_dct = {
 def sent_time_finder(sent):
     """ Simple time lookup from string.
 
+    (0) Set time to 0. Check what's in sentence. Look for 'every'. Loop over all words in sentence.
     (1) Looks for times_dct keys in sent. If found checks if int before key.
-    If int not found set num = 1 else set num = int. Multiply num by key's value. Check for 'every'.
+    If int not found set num = 1 else set num = int. Multiply num by key's value.
     (2) Same as (1) but for days_dct.
     (3) If word 'at' in sent assume 00:00:00 time format follows (can be 00 or 00:00 or 00:00:00).
     (4) If word 'on' in sent assume specific date follows in format d m y.
@@ -67,19 +68,78 @@ def sent_time_finder(sent):
     Returns
     -------
     Returns time TO event.
-    if filter_times:
-        (int for time in string, word2vec_input list with no times, True if every in string else False)
-    else:
-        (int for time in string, True if every in string else False)
+        (t, every)
+        (int for time in string, False or int)
+        Returned int in t variable is time to first message.
+        Returned int in every variable is time to next message
 
     Note
     ----
-    Naive (bad) implementation. Timezones not supported, works with local only.
+    Naive (bad) implementation. Timezones not supported, works with local time only.
 
     Examples
     --------
-    sent = 'remind me on 17.8.19 at 17:00' -> (3796550, False)
-    sent = 'remind me every day' -> (86400, True)
+    now time: 11.08.2019 (sun) @ 20:33:37
+
+    string: tomorrow
+    date: 12.08.2019 @ 00:00:00
+    time to: 3 h, 26 min, 23 sec
+    -----------------------------
+    string: tomorrow at 12
+    date: 12.08.2019 @ 12:00:00
+    time to: 15 h, 26 min, 23 sec
+    -----------------------------
+    string: 1 day
+    date: 12.08.2019 @ 20:33:37
+    time to: 1 day
+    -----------------------------
+    string: 2 days at 22:00
+    date: 14.08.2019 @ 22:00:00
+    time to: 3 days, 1 h, 26 min, 23 sec
+    -----------------------------
+    string: fri
+    date: 16.08.2019 @ 00:00:00
+    time to: 4 days, 3 h, 26 min, 23 sec
+    -----------------------------
+    string: mon
+    date: 12.08.2019 @ 00:00:00
+    time to: 3 h, 26 min, 23 sec
+    -----------------------------
+    string: sat at 9
+    date: 17.08.2019 @ 09:00:00
+    time to: 5 days, 12 h, 26 min, 23 sec
+    -----------------------------
+    string: 10 sec
+    date: 11.08.2019 @ 20:33:47
+    time to: 10 sec
+    -----------------------------
+    string: 1 week
+    date: 18.08.2019 @ 20:33:37
+    time to: 1 week
+    -----------------------------
+    string: at 23:50
+    date: 11.08.2019 @ 23:50:00
+    time to: 3 h, 16 min, 23 sec
+    -----------------------------
+    string: every day at 22
+    date: 12.08.2019 @ 22:00:00
+    time to: 1 day, 1 h, 26 min, 23 sec
+    -----------------------------
+    string: every 2 days at 14
+    date: 13.08.2019 @ 14:00:00
+    time to: 1 day, 17 h, 26 min, 23 sec
+    -----------------------------
+    string: every week at 7
+    date: 18.08.2019 @ 07:00:00
+    time to: 6 days, 10 h, 26 min, 23 sec
+    -----------------------------
+    string: every 10 h
+    date: 12.08.2019 @ 06:33:37
+    time to: 10 h
+    -----------------------------
+    string: every day
+    date: 12.08.2019 @ 20:33:37
+    time to: 1 day
 
     """
     vec_sent = word2vec_input(sent)
@@ -123,13 +183,18 @@ def sent_time_finder(sent):
 
         elif w == "at":
 
-            if "on" not in true_sent and 'day' not in in_sent:
-                to_midnight = 86400 - (now.hour * 3600 + now.minute * 60 + now.second)
+            if every:
+                every = t
 
-                if 'tomorrow' in true_sent:
-                    t = 0
+            if 'tomorrow' in true_sent:
+                t = 0
+                t += 86400 - (now.hour * 3600 + now.minute * 60 + now.second)
 
-                t += to_midnight
+            if in_sent['time'] in ['day', 'days', 'd', 'week', 'weeks', 'w'] and in_sent['on'] is False:
+                t += 86400 - (now.hour * 3600 + now.minute * 60 + now.second)
+
+                if every:
+                    t -= 86400
 
             for j, hms in enumerate(true_sent[i + 1:]):
 
@@ -145,10 +210,13 @@ def sent_time_finder(sent):
                 else:
                     break
 
+            if in_sent['day'] is None and in_sent['time'] is None:
+                t -= now.hour * 3600 + now.minute * 60 + now.second
+
         elif w == "on":
 
-            # check if on before day word (if True this is not it and pass)
-            if 'day' not in in_sent:
+            # if user writes on <day> skip this
+            if in_sent['day'] is None:
                 d = 0
                 m = now.month
                 y = now.year
@@ -177,26 +245,32 @@ def sent_time_finder(sent):
         else:
             continue
 
+    if every is True:
+        every = t
+
+    if t < 0:
+        t = None
+
     return t, every
 
 
 def whats_in_sent(true_sent):
 
-    in_sent = []
+    in_sent = {'time': None, 'day': None, 'on': False, 'at': False}
 
     for word_ in true_sent:
         for time_ in times_dct:
             if word_ == time_:
-                in_sent.append('time')
+                in_sent['time'] = word_
 
         for day_ in days_dct:
             if word_ == day_:
-                in_sent.append('day')
+                in_sent['day'] = word_
 
         if word_ == 'on':
-            in_sent.append('on')
+            in_sent['on'] = True
         elif word_ == 'at':
-            in_sent.append('at')
+            in_sent['at'] = True
         else:
             pass
 
@@ -258,9 +332,12 @@ def convert_sec(seconds):
     return ', '.join(result)
 
 
-# ex1 = ['tomorrow', 'tomorrow at 12', '1 day', '2 days at 22:00', 'fri', 'mon', 'sat at 9', '10 sec', '1 week']
+# ex1 = ['tomorrow', 'tomorrow at 12', '1 day', '2 days at 22:00', 'fri', 'mon', 'sat at 9', '10 sec', '1 week', 'at 23:50',
+#        'every day at 22', 'every 2 days at 14', 'every week at 7', 'every 10 h', 'every day']
+#
 # for e in ex1:
-#     print(e)
+#     print('string:', e)
 #     _ = sent_time_finder(e)[0]
-#     print(timestamp_to_local(_ + time.time()))
+#     print('date:', timestamp_to_local(_ + time.time()))
+#     print('time to:', convert_sec(_))
 #     print('-----------------------------')
