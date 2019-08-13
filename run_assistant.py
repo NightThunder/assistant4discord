309b6b5fca8d7a68c3b3597c3d846421754b2abc
NightThunder <night.thunderx@gmail.com>
2019-08-13 20:19:14 +0200

    Heroku files

---
+++ run_assistant.py
@@ -0,0 +1,10 @@
+from assistant4discord.discord_api.discord_client import run
+import os
+
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+MONGODB_TOKEN = os.environ['MONGODB_TOKEN']
+
+
+run(method="tf", discord_token=DISCORD_TOKEN, mongodb_token=MONGODB_TOKEN, log_chat=False)
+# run(method="w2v", model_name="model_v1.kv", discord_token=DISCORD_TOKEN, mongodb_token=MONGODB_TOKEN, log_chat=False)
