351ae730d549ef9f301513f5f661539a1cc94860
NightThunder <night.thunderx@gmail.com>
2019-08-21 22:41:35 +0200

    from class to function

--- assistant4discord/assistant/commands/helpers/web_checker.py
+++ assistant4discord/assistant/commands/helpers/web_checker.py
@@ -1,10 +1,26 @@
 import aiohttp
 import asyncio
 from aiohttp.client_exceptions import InvalidURL
-from assistant4discord.assistant.commands.master.master_class import Master
 
 
-class WebChecker(Master):
+async def fetch(session, url):
+    try:
+        async with session.get(url) as response:
+            if response.status != 200:
+                response.raise_for_status()
+
+            return await response.text()
+
+    except InvalidURL:
+        return None
+
+
+async def fetch_all(session, urls, client):
+    results = await asyncio.gather(*[client.loop.create_task(fetch(session, url)) for url in urls])
+    return results
+
+
+async def get_content(urls, client):
     """ Get content of a website.
 
     Note
@@ -16,29 +32,12 @@ class WebChecker(Master):
     https://aiohttp.readthedocs.io/en/stable/client_reference.html
     https://stackoverflow.com/questions/35879769/fetching-multiple-urls-with-aiohttp-in-python-3-5
     https://faust.readthedocs.io/en/latest/_modules/aiohttp/client.html
+
+    Returns
+    -------
+    List of html of each url.
+
     """
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    @staticmethod
-    async def fetch(session, url):
-
-        try:
-            async with session.get(url) as response:
-                if response.status != 200:
-                    response.raise_for_status()
-
-                return await response.text()
-
-        except InvalidURL:
-            return None
-
-    async def fetch_all(self, session, urls):
-        results = await asyncio.gather(*[self.client.loop.create_task(self.fetch(session, url)) for url in urls])
-        return results
-
-    async def get_content(self, urls):
-
-        async with aiohttp.ClientSession() as session:
-            htmls = await self.fetch_all(session, urls)
-            return htmls
+    async with aiohttp.ClientSession() as session:
+        htmls = await fetch_all(session, urls, client)
+        return htmls
