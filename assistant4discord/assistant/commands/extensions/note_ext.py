a603cb17a7f665fa39ed5ffefb398f4f777b120d
NightThunder <night.thunderx@gmail.com>
2019-09-18 17:09:38 +0200

    minor __str__ change

--- assistant4discord/assistant/commands/extensions/note_ext.py
+++ assistant4discord/assistant/commands/extensions/note_ext.py
@@ -20,4 +20,4 @@ class Note(Extend):
         self.noted = self.get_message()
 
     def __str__(self):
-        return "noted on {}\nnote: {}".format(timestamp_to_local(self.created_on), self.noted)
+        return "noted on {}\n{}".format(timestamp_to_local(self.created_on), self.noted)
